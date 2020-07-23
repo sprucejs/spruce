c6d231e277fb4452f750f68306c53af6b147c9d0
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-07-23 12:25:23 +0100

    add ability to decode jwt

--- modules/auth/services/spruce-auth.service.ts
+++ modules/auth/services/spruce-auth.service.ts
@@ -1,6 +1,7 @@
 import { compare, hash } from 'bcryptjs';
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 import { injectable } from 'tsyringe';
+import { promisify } from 'util';
 
 import { IJwtConfig } from '../interfaces/jwt-config.interface';
 
@@ -21,5 +22,28 @@ export class SpruceAuthService {
     return sign(jwtConfig.payload, jwtConfig.secret, {
       expiresIn: jwtConfig.expiresIn
     });
+  }
+
+  public decodeJwt(jwtToken: string, secret: string): Promise<IJwtConfig> {
+    const bearerToken = this._getToken(jwtToken);
+    const jwtVerify: any = promisify(verify);
+
+    if (!bearerToken) {
+      return Promise.reject();
+    } else {
+      return jwtVerify(bearerToken, secret);
+    }
+  }
+
+  private _getToken(auth: string): string | null {
+    let token: string;
+
+    if (auth && auth.startsWith('Bearer')) {
+      token = auth.split(' ')[1];
+    } else {
+      return null;
+    }
+
+    return token;
   }
 }
