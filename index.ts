658cb3ea1b862ce8042957d0f5f8b79416fb9e1a
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-08-15 19:07:29 +0100

    move index ts and update main entry point in package

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from './modules/app';
+export * from './modules/auth';
+export * from './modules/errors';
+export * from './modules/http';
+export * from './modules/internal';
+export * from './modules/routing';
+export * from './modules/testing';
--- modules/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './app';
-export * from './auth';
-export * from './errors';
-export * from './http';
-export * from './internal';
-export * from './routing';
-export * from './testing';
