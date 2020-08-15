a07870054afeab2486d0cfe63d5563514d8632e0
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-08-15 19:00:47 +0100

    add missing exports

---
+++ modules/index.ts
@@ -0,0 +1,7 @@
+export * from './app';
+export * from './auth';
+export * from './errors';
+export * from './http';
+export * from './internal';
+export * from './routing';
+export * from './testing';
